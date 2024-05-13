14ddfa6f9b9114bad5f85ecd89c715474bc43913
SchaafLaura <schaaf.laura92@gmail.com>
2024-05-13 18:16:41 +0200

    Add project files.

---
+++ TLML_SC/GameSettings.cs
@@ -0,0 +1,5 @@
+﻿internal static class GameSettings
+{
+    public const int GAME_WIDTH = 90;
+    public const int GAME_HEIGHT = 30;
+}
---
+++ TLML_SC/Program.cs
@@ -0,0 +1,14 @@
+﻿using SadConsole.Configuration;
+
+Settings.WindowTitle = "My SadConsole Game";
+
+Builder gameStartup = new Builder()
+    .SetScreenSize(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+    .SetStartingScreen<TLML_SC.Scenes.RootScreen>()
+    .IsStartingScreenFocused(true)
+    .ConfigureFonts(true)
+    ;
+
+Game.Create(gameStartup);
+Game.Instance.Run();
+Game.Instance.Dispose();
